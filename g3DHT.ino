2fa8a52a3afc3c4b15aa5325634d537481f8a532
radhanand <48708906+radhanand@users.noreply.github.com>
2019-03-19 14:44:21 +0530

    Add files via upload

---
+++ g3DHT.ino
@@ -0,0 +1,19 @@
+#include <dht.h>
+
+dht DHT;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int chk = DHT.read11(6);
+Serial.println("Ambient temperature = " + String(DHT.temperature)+" deg. C");
+Serial.println("Relative humidity   = " + String(DHT.humidity)+" %");
+Serial.println("_______________________________");
+
+delay(3000);
+
+}
